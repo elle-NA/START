fe4b7acc522027e7e60e8f16a623903d9118719c
elle-NA <ellena.0303@gmail.com>
2021-10-27 20:07:38 +0300

    drd

--- START/START/Program.cs
+++ START/START/Program.cs
@@ -7,6 +7,7 @@ namespace START
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("");
             Console.WriteLine("Hello Misho");
         }
     }
